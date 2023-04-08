e7b9e30f823cf14dc0d66b2e2d7a2fe22ee7af0d
AnnoyingRains <annoyingrain5@gmail.com>
2023-04-09 00:39:12 +1000

    Switch to new #QnA-discussion channel system

--- bot.py
+++ bot.py
@@ -5,14 +5,16 @@ from discord.ext.commands import CheckFailure
 import sys
 import traceback
 from io import StringIO
+from discord.commands.context import ApplicationContext as SlashContext
 
 load_dotenv()  # load env vars from file
 
 # set up intents
 intents = discord.Intents.default()
 intents += discord.Intents.message_content
-
 bot = discord.Bot(intents=intents)
+
+QNA_CHANNEL_ID = 1094266918556422204
 
 
 @bot.event
@@ -22,14 +24,17 @@ async def on_ready():
 
 
 @bot.event
-async def on_message(ctx):
+async def on_message(ctx: discord.Message):
     if ctx.author == bot.user:
         return
 
     # if the channel is polls
-    if ctx.channel.id == 981343621104140308:
-        await ctx.add_reaction(":vyes:1054227682944110612")
-        await ctx.add_reaction(":vno:1054227685016092833")
+    if ctx.channel.id == QNA_CHANNEL_ID:
+        if ctx.content.endswith("?"):
+            response = f"Hey {ctx.author.mention}!\n"
+            response += "If this is a question of the day, please mark it as such by using /ask!\n"
+            response += "This message will self destruct in 10 seconds"
+            await ctx.channel.send(response, delete_after=10)
 
     # if the channel is artwork showcase
     if ctx.channel.id == 809238709635383327:
@@ -42,23 +47,67 @@ async def on_message(ctx):
         info = await bot.application_info()
         await info.owner.send("You have a new DM from " + ctx.author.name + " (" + ctx.author.mention + "):\n" + ctx.content)
 
+
 # main commands
 
-
 @bot.slash_command(description="Ping the bot!")
-async def ping(ctx):
+async def ping(ctx: SlashContext):
     await ctx.respond(f"Pong! {round(bot.latency * 1000)}ms")
 
 
 @bot.slash_command(description="Get the source code!")
-async def github(ctx):
+async def github(ctx: SlashContext):
     await ctx.respond("check out my source code here: https://github.com/AnnoyingRain5/Victini-Guard, and send a PR if you're feeling kind!")
+
+
+@bot.slash_command(description="Format a question for the QnA channel!")
+async def ask(ctx: SlashContext, question: str):
+    if not isinstance(ctx.channel, discord.TextChannel):
+        await ctx.respond("This command can only be run in text channels")
+        return
+    if ctx.channel.id != QNA_CHANNEL_ID:
+        await ctx.respond(f"This needs to be used in the {bot.get_channel(QNA_CHANNEL_ID).mention} channel!", ephemeral=True)
+        return
+    embed = discord.Embed()
+    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+    embed.title = question
+    embed.set_footer(text=f"New question by {ctx.author.name}")
+    embed.color = 0x20a303
+    await ctx.respond(embed=embed)
+
+
+@bot.slash_command(description="Set up a poll!")
+async def poll(ctx: SlashContext, poll: str):
+    # ensure we are in a text channel
+    if not isinstance(ctx.channel, discord.TextChannel):
+        await ctx.respond("This command can only be run in text channels")
+        return
+    # ensure we are in the polls channel
+    if ctx.channel.id != QNA_CHANNEL_ID:
+        await ctx.respond(f"This needs to be used in the {bot.get_channel(QNA_CHANNEL_ID).mention} channel!", ephemeral=True)
+        return
+    embed = discord.Embed()
+    embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar)
+    embed.title = poll
+    embed.set_footer(text=f"New poll by {ctx.author.name}")
+    embed.color = 0x2b34cf
+    await ctx.respond(embed=embed)
+    # Find the message that we just posted in the history
+    async for message in ctx.channel.history():
+        if not message.embeds:
+            continue
+        if message.embeds[0].author.name == embed.author.name and message.embeds[0].title == embed.title:
+            # React to that message
+            await message.add_reaction(":vyes:1054227682944110612")
+            await message.add_reaction(":vno:1054227685016092833")
+            break  # ensure we don't react more than once
+
 
 # admin commands
 
 
 @bot.slash_command(description="owner only command")
-async def senddm(ctx, user: discord.User, message: str):
+async def senddm(ctx: SlashContext, user: discord.User, message: str):
     info = await bot.application_info()
     if ctx.author == info.owner:
         await user.send(message)
@@ -68,7 +117,7 @@ async def senddm(ctx, user: discord.User, message: str):
 
 
 @bot.slash_command(description="owner only command")
-async def changestatus(ctx, status: str):
+async def changestatus(ctx: SlashContext, status: str):
     info = await bot.application_info()
     if ctx.author == info.owner:
         await bot.change_presence(activity=discord.Game(name=status))
